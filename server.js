399c32d4fef70acf95c53994804af3586dc767d7
claire p <cpuckett9@gmail.com>
2020-12-14 16:11:54 -0600

    Deploying

--- server.js
+++ server.js
@@ -16,9 +16,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+// mongoose.connect(MONGODB_URI, {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget-tracker', {
+  useFindAndModify: false,
   useNewUrlParser: true,
-  useFindAndModify: false
+  useUnifiedTopology: true
 });
 
 // routes
