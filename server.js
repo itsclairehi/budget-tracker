cfa794908c1c333bfa404fe5dbaeaef0df86df08
claire p <cpuckett9@gmail.com>
2020-12-14 16:15:52 -0600

    troubleshooting

--- server.js
+++ server.js
@@ -16,16 +16,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// mongoose.connect(MONGODB_URI, {
-//   useNewUrlParser: true,
-//   useFindAndModify: false
-// });
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget-tracker', {
-  useFindAndModify: false,
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useFindAndModify: false
 });
+
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget-tracker', {
+//   useFindAndModify: false,
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// });
 
 // routes
 app.use(require("./routes/api.js"));
